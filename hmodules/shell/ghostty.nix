e61e961b59d0b4010afc7dc43eb2341bd9ae7216
EthanIngraham <ethaningraham2052@outlook.com>
2025-12-06 00:20:31 +0000

    transparent ghostty

--- hmodules/shell/ghostty.nix
+++ hmodules/shell/ghostty.nix
@@ -7,8 +7,11 @@
       theme = "catppuccin-mocha";
 
       font-family = "JetBrains Mono";
-      font-size = 15; 
-      
+      font-size = 15;
+
+      background-opacity = 0.85;
+      background-blur-radius = 20;
+
       font-thicken = true;
 
       "window-padding-x" = 10;
