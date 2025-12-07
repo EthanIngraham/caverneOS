5b996093bb2ac880caf69a0c5527ecf9b88ac6ab
EthanIngraham <ethaningraham2052@outlook.com>
2025-12-07 17:05:21 +0000

    hyprlock (2)

--- hmodules/desktop/hyprlock.nix
+++ hmodules/desktop/hyprlock.nix
@@ -11,6 +11,36 @@
         path = "/home/caverne/Pictures/the_artists_garden.jpg";
         blur_passes = 3;
       }];
+
+      input-field = [{
+        size = "300, 50";
+        outline_thickness = 2;
+        dots_size = 0.2;
+        dots_spacing = 0.2;
+        dots_center = true;
+        
+        outer_color = "rgb(137, 180, 250)";  
+        inner_color = "rgb(30, 30, 46)";    
+        font_color = "rgb(205, 214, 244)"; 
+        
+        fade_on_empty = false;
+        placeholder_text = "Enter Password...";
+        
+        position = "0, -120";
+        halign = "center";
+        valign = "center";
+      }];
+      
+      label = [{
+        text = "Hi, $USER";
+        color = "rgb(205, 214, 244)";
+        font_size = 25;
+        font_family = "JetBrains Mono";
+        
+        position = "0, -40";
+        halign = "center";
+        valign = "center";
+      }];
     };
   };
 }
